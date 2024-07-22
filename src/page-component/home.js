e2494ca30b526d7e67c57d831950571899df5d71
ZenxyNC <timothynathanlucas@gmail.com>
2024-07-22 11:25:14 +0700

    Quotes, sched layout, home layout

--- src/page-component/home.js
+++ src/page-component/home.js
@@ -6,12 +6,24 @@ import zen from './Icon.png'
 export default function Home() {
   const [username, setUsername] = useState(localStorage.getItem('user'))
   const [isLoggedIn, setLogs] = useState(localStorage.getItem('isLoggedIn'))
+  const [quotes, setQuotes] = useState();
+  const [isQuotes, setIsQuotes] = useState(false) 
+  const [quotesState, setQuoteState] = useState("Quotes of the day")
   const navigate = useNavigate()
 
   if (isLoggedIn) {
 
   } else {
     navigate('/zencourse/login', {replace : true})
+  }
+  function setQuotesStatus() {
+    if (isQuotes) {
+      setIsQuotes(false)
+      setQuoteState("Quotes of the day")
+    } else {
+      setIsQuotes(true)
+      setQuoteState("Don't learn only from school!")
+    }
   }
   
   return (
@@ -44,10 +56,14 @@ export default function Home() {
               <button className='utilities-button'>Task</button><br/>
               <button className='utilities-button'>Presence</button>
             </div>
+            <div id='quotesOfTheDay' onClick={setQuotesStatus}>
+              <div className={`quotesButton ${isQuotes ? 'opened' : 'idle'}`}>
+                {quotesState}
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </>
   )
-
 }
